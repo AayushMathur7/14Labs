d4eaee00a50764ec6264dfd4ad7a0f7e36fbcf82
zac <zacchaeuschok2014@gmail.com>
2023-11-18 22:38:10 -0800

    Resolve merge conflicts

--- app.py
+++ app.py
@@ -1,20 +1,70 @@
-from llama_hub.web.news import NewsArticleReader
 from logo import render_logo
 from assistant import PodcastAssistant
 import streamlit as st
 import openai
 import uuid
 import time
+import pandas as pd
 from openai import OpenAI
+
 import os
 from text_to_speech import generate_text_to_speech
 import asyncio
 import nest_asyncio
+
 nest_asyncio.apply()
+
+from tavily import TavilyClient
+from llama_hub.web.news import NewsArticleReader
+import re
+
+
+def fetch_and_append_articles(urls):
+    # Initialize an empty DataFrame if it doesn't exist in the session state
+    if "articles" not in st.session_state:
+        st.session_state["articles"] = pd.DataFrame(
+            columns=["Title", "Publish Date", "URL"]
+        )
+
+    new_articles = []
+    for url in urls:
+        # Escape special characters for regular expression
+        escaped_url = re.escape(url)
+
+        # Check if the URL is already in the DataFrame
+        if (
+            not st.session_state["articles"]["URL"]
+            .str.contains(escaped_url, regex=True)
+            .any()
+        ):
+            try:
+                # Fetch document data
+                documents = reader.load_data([url])
+
+                # Extract relevant data
+                for doc in documents:
+                    new_article = {
+                        "Title": doc.metadata.get("title", "No Title"),
+                        "Publish Date": doc.metadata.get(
+                            "publish_date", "No Date"
+                        ),
+                        "URL": doc.metadata.get("link", url),
+                    }
+                    new_articles.append(new_article)
+            except Exception as e:
+                st.error(f"An error occurred while fetching {url}: {e}")
+
+    # Update session state
+    if new_articles:
+        new_df = pd.DataFrame(new_articles)
+        st.session_state["articles"] = pd.concat(
+            [st.session_state["articles"], new_df], ignore_index=True
+        )
 
 
 # Initialize OpenAI client
 client = OpenAI()
+tavily_client = TavilyClient(api_key=st.secrets["TAVILY_API_KEY"])
 
 # Your chosen model
 MODEL = "gpt-4-1106-preview"
@@ -39,7 +89,8 @@ st.sidebar.markdown("### Configurations")
 
 # File uploader for CSV, XLS, XLSX
 uploaded_file = st.file_uploader(
-    "Upload your file", type=["csv", "xls", "xlsx"])
+    "Upload your file", type=["csv", "xls", "xlsx"]
+)
 
 duration = st.sidebar.selectbox(
     "Choose Podcast Duration", ("1 minute", "5 minutes", "15 minutes")
@@ -54,15 +105,17 @@ host = st.sidebar.radio(
     ),
 )
 
-if st.button('Generate Podcast'):
-    mp3_path = asyncio.run(generate_text_to_speech(
-        host,
-        "Hello! My name is Eleven. I am a virtual assistant that can help you generate a podcast episode."
-    ))
+if st.button("Generate Podcast"):
+    mp3_path = asyncio.run(
+        generate_text_to_speech(
+            host,
+            "Hello! My name is Eleven. I am a virtual assistant that can help you generate a podcast episode.",
+        )
+    )
     if mp3_path is not None:
-        audio_file = open(mp3_path, 'rb')
+        audio_file = open(mp3_path, "rb")
         audio_bytes = audio_file.read()
-        st.audio(audio_bytes, format='audio/mp3')
+        st.audio(audio_bytes, format="audio/mp3")
 
 genres = st.sidebar.multiselect(
     "Select Podcast Genres",
@@ -78,59 +131,60 @@ As a podcast generator assistant, you are tasked with creating a personalized po
 
 You are supposed to generate a podcast episode that is {duration} long, hosted by {host}, and covers topics in {' and '.join(genres)}. The episode will reflect the user's chosen genres, ensuring a tailored and engaging listening experience.
 
-When the user provides URLs, you should read the content of the URL, and use that as content for the podcast episode.
+You may also receive news content from web URLs, which should inform the content of your porcast script.
 
 You will only generate the podcast when the user types "/generate".
+
+You will use the provided Tavily search API function to find relevant news article URLs to supplement your content when the user types "/news" followed by the topic
+
+Do not use Tavily search API function otherwise.
+
+Do not generate the podcast otherwise.
+
+If you are not generating the podcast, you should respond to the user's chat messages with your findings.
+
+When you generate the podcast, you should only provide the transcript of the podcast itself. Your response should not incorporate any bullshit before or after the content of the transcript.
 """
 
-podcast_assistant = PodcastAssistant(client, PROMPT)
+podcast_assistant = PodcastAssistant(client, tavily_client, PROMPT)
+# podcast_assistant = {"assistant": {"id": "asst_WHdn0UmJCvQHfXWpwtcYVau2"}}
 
 # Initialize the NewsArticleReader
 reader = NewsArticleReader(use_nlp=False)
 
-# Streamlit interface
-st.title("News Article Retriever")
+# Initialize session state for storing articles
+if "articles" not in st.session_state:
+    st.session_state.articles = pd.DataFrame(
+        columns=["Title", "Publish Date", "URL"]
+    )
 
-# Input for URLs
-urls_input = st.text_area("Enter URLs (one per line)")
-urls = urls_input.strip().split("\n")
+# Input for single URL
+col1, col2 = st.columns([8, 2])
 
-# Button to fetch news
-if st.button("Fetch News"):
-    if urls:
-        try:
-            documents = reader.load_data(urls)
+with col1:
+    url = st.text_input(
+        label="**Add article**", placeholder="Add URL for your podcast content"
+    )
 
-            # Display each article and its metadata
-            for doc in documents:
-                st.subheader(doc.metadata.get("title", "No Title"))
-                st.write("URL:", doc.metadata.get("link", "No URL"))
-                st.write(
-                    "Authors:", ", ".join(doc.metadata.get("authors", []))
-                )
-                st.write(
-                    "Publish Date:",
-                    doc.metadata.get("publish_date", "No Date"),
-                )
-                st.write(
-                    "Description:",
-                    doc.metadata.get("description", "No Description"),
-                )
-                st.write("Content:", doc.text)
-                st.write(
-                    "Keywords:", ", ".join(doc.metadata.get("keywords", []))
-                )
-                st.write("Summary:", doc.metadata.get("summary", "No Summary"))
-                st.markdown("---")
+with col2:
+    st.markdown("")
+    fetch_news = st.button("Fetch News")
 
-        except Exception as e:
-            st.error(f"An error occurred: {e}")
+if fetch_news and url:
+    fetch_and_append_articles([url])
+
+with st.expander("**Article List**"):
+    if "articles" in st.session_state and not st.session_state.articles.empty:
+        st.data_editor(
+            st.session_state.articles, hide_index=True, num_rows="dynamic"
+        )
 
 # Initialize OpenAI assistant
 if "assistant" not in st.session_state:
     openai.api_key = st.secrets["OPENAI_API_KEY"]
     st.session_state.assistant = openai.beta.assistants.retrieve(
-        podcast_assistant.assistant.id
+        # podcast_assistant.assistant.id
+        "asst_WHdn0UmJCvQHfXWpwtcYVau2"
     )
     st.session_state.thread = client.beta.threads.create(
         metadata={"session_id": st.session_state.session_id}
@@ -144,17 +198,52 @@ elif (
     st.session_state.messages = client.beta.threads.messages.list(
         thread_id=st.session_state.thread.id
     )
+
     for message in reversed(st.session_state.messages.data):
-        if message.role in ["user", "assistant"]:
+        if message.role in ["user", "assistant"] and not message.content[
+            0
+        ].text.value.startswith("Title:"):
             with st.chat_message(message.role):
                 for content_part in message.content:
                     message_text = content_part.text.value
                     st.markdown(message_text)
 
+                    web_links = podcast_assistant.extract_urls(message_text)
+
+                    if web_links:
+                        fetch_and_append_articles(web_links)
+
 # Chat input and message creation with file ID
-if prompt := st.chat_input("How can I help you?"):
+if prompt := st.chat_input("Type /generate to create podcast"):
     with st.chat_message("user"):
         st.write(prompt)
+
+    if prompt == "/generate":
+        news_content = ""
+
+        if not st.session_state.articles.empty:
+            for _, row in st.session_state.articles.iterrows():
+                url = row["URL"]
+                try:
+                    # Reload content for each URL
+                    document = reader.load_data([url])[0]
+                    title = document.metadata.get("title", "No Title")
+                    content = document.text
+                    news_content += f"Title: {title}\nContent: {content}\n\n"
+                except Exception as e:
+                    st.error(
+                        f"An error occurred while fetching content for {url}: {e}"
+                    )
+
+            message_data = {
+                "thread_id": st.session_state.thread.id,
+                "role": "user",
+                "content": news_content,
+            }
+
+            st.session_state.messages = client.beta.threads.messages.create(
+                **message_data
+            )
 
     message_data = {
         "thread_id": st.session_state.thread.id,
@@ -183,6 +272,19 @@ if hasattr(st.session_state.run, "status"):
     if st.session_state.run.status == "running":
         with st.chat_message("assistant"):
             st.write("Thinking ......")
+        if st.session_state.retry_error < 3:
+            time.sleep(1)
+            st.rerun()
+
+    elif st.session_state.run.status == "requires_action":
+        st.session_state.run = podcast_assistant.submit_tool_outputs(
+            client,
+            st.session_state.thread.id,
+            st.session_state.run.id,
+            st.session_state.run.required_action.submit_tool_outputs.tool_calls,
+        )
+        with st.chat_message("assistant"):
+            st.write("Fetching the latest news ......")
         if st.session_state.retry_error < 3:
             time.sleep(1)
             st.rerun()
--- assistant.py
+++ assistant.py
@@ -1,17 +1,73 @@
-from openai import OpenAI
-from dotenv import load_dotenv
+import json
+import re
 
-load_dotenv()
+
+tavily_search_tool = {
+    "type": "function",
+    "function": {
+        "name": "tavily_search",
+        "description": "Get information on recent events from the web.",
+        "parameters": {
+            "type": "object",
+            "properties": {
+                "query": {
+                    "type": "string",
+                    "description": "The search query to use. For example: 'Latest news on Nvidia stock performance'",
+                },
+            },
+            "required": ["query"],
+        },
+    },
+}
 
 
 class PodcastAssistant:
-    def __init__(self, client, prompt, model="gpt-4-1106-preview"):
+    def __init__(
+        self, client, tavily_client, prompt, model="gpt-4-1106-preview"
+    ):
+        self._tavily_client = tavily_client
         self._client = client
         self._model = model
         self._prompt = prompt
         self.assistant = self._client.beta.assistants.create(
             name="Podcast GPT",
             instructions=self._prompt,
-            tools=[{"type": "retrieval"}],
+            tools=[tavily_search_tool],
             model=self._model,
         )
+
+    def tavily_search(self, query):
+        search_result = self._tavily_client.get_search_context(
+            query, search_depth="advanced", max_tokens=8000
+        )
+        return search_result
+
+    def submit_tool_outputs(self, client, thread_id, run_id, tools_to_call):
+        tool_output_array = []
+
+        for tool in tools_to_call:
+            output = None
+            tool_call_id = tool.id
+            function_name = tool.function.name
+            function_args = tool.function.arguments
+
+            if function_name == "tavily_search":
+                output = self.tavily_search(
+                    query=json.loads(function_args)["query"]
+                )
+
+            if output:
+                tool_output_array.append(
+                    {"tool_call_id": tool_call_id, "output": output}
+                )
+
+        return client.beta.threads.runs.submit_tool_outputs(
+            thread_id=thread_id, run_id=run_id, tool_outputs=tool_output_array
+        )
+
+    def extract_urls(self, text):
+        # Regex pattern for extracting URLs
+        url_pattern = r"https?://(?:[-\w.]|(?:%[\da-fA-F]{2}))+[^ \n]*"
+        # Find all matches in the text
+        urls = re.findall(url_pattern, text)
+        return urls
